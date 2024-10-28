3610d3ecc296c64218e17b04d6de062182137903
PhoebeLiuyf <liuyufeilucky24@163.com>
2024-10-28 23:11:59 +0000

    用countrycode后转成了value 找解决办法中

--- hmwk4.R
+++ hmwk4.R
@@ -1,1 +1,27 @@
+install.packages("countrycode")
+install.packages("remotes")
+library(remotes)
+install_github('vincentarelbundock/countrycode')
 library(sf)
+library(jsonlite)
+library(tidyverse)
+library(dplyr)
+# 读取 JSON 文件
+gddf <- fromJSON("/Users/xiaoqianmai/Desktop/GIS/week4/hmwk/hdr-data.json")
+'''
+gddf2010 <- gddf %>%
+  filter(year==2010)
+gddf2019 <- gddf %>%
+  filter(year==2019)
+'''
+sf <-read_sf("/Users/xiaoqianmai/Desktop/GIS/week4/hmwk/World_Countries_(Generalized)_2402777631520798174/World_Countries_Generalized.shp")
+#把iso换为3位代码
+library(countrycode)
+sf3<- countrycode(sf$ISO, origin = 'iso2c', destination = 'iso3c')
+
+
+gddfw <-gddf %>%
+  pivot_wider(.,names_from = "year",values_from = "value" ) %>%
+  left_join(.,sf3,by=c("countryIsoCode"="ISO")) %>%
+  mutate(difference = `2019`-`2010`)
+
